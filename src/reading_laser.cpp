57c224908ab3eedf6496e95ecf4e42c42dc97f18
Alessandro <alesc98@gmail.com>
2020-04-14 18:04:10 +0200

    modifica al nodo reading_laser

--- src/reading_laser.cpp
+++ src/reading_laser.cpp
@@ -3,11 +3,90 @@
 #include <sstream>
 #include <string>
 #include "sensor_msgs/LaserScan.h"
-#include <sstream>
 #include <iomanip>
+/*
+funzione per il calcolo del minimo in un array 
+Parametri: a[] array di float , n1 indice 1, n2 indice 2
 
+float calcola_minimo(const std::vector<float> a,int n1,int n2)
+{
+    int i;
+    float minimo = a[n1];
+    for(i=n1+1;i<=n2;i++)
+        if(a[i]<minimo)
+            minimo = a[i];
+    return minimo;
+
+}
+*/
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
+
+    //Divisione dei 180 gradi con 720 letture in 6 regioni uguali
+    //Calcolo del minimo per ogni regione
+    float min_set1=0;
+    float min_set2=0;
+    float min_set3=0;
+    float min_set4=0;
+    float min_set5=0;
+    float min_set6=0;
+    int i;
+    
+    min_set1 = msg->ranges[0];
+    for(i=0;i<120;i++)
+    {
+        if(msg->ranges[i] < min_set1)
+            min_set1 = msg->ranges[i];
+    }
+    if(min_set1>10)
+        min_set1 = 10;
+
+    min_set2 = msg->ranges[120];
+    for(i=120;i<240;i++)
+    {
+        if(msg->ranges[i] < min_set2)
+            min_set2 = msg->ranges[i];
+    }
+    if(min_set2>10)
+        min_set2 = 10;
+
+    min_set3 = msg->ranges[240];
+    for(i=240;i<360;i++)
+    {
+        if(msg->ranges[i] < min_set3)
+            min_set3 = msg->ranges[i];
+    }
+    if(min_set3>10)
+        min_set3 = 10;
+    
+    min_set4 = msg->ranges[360];
+    for(i=360;i<480;i++)
+    {
+        if(msg->ranges[i] < min_set4)
+            min_set4 = msg->ranges[i];
+    }
+    if(min_set4>10)
+        min_set4 = 10;
+    
+    min_set5 = msg->ranges[480];
+    for(i=480;i<600;i++)
+    {
+        if(msg->ranges[i] < min_set5)
+            min_set5 = msg->ranges[i];
+    }
+    if(min_set5>10)
+        min_set5 = 10;
+
+    min_set6 = msg->ranges[600];
+    for(i=600;i<720;i++)
+    {
+        if(msg->ranges[i] < min_set6)
+            min_set6 = msg->ranges[i];
+    }
+    if(min_set6>10)
+        min_set6 = 10;
+
+/*Inserimento elementi in un unica stringa
     std::string s;
     std::string a;
     int i;
@@ -30,7 +109,9 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
         ss.str("");
         a.clear();
     }
-   ROS_INFO("\n[Angolo minimo: %f]\n[Angolo massimo: %f]\n[Passo: %f]\n[Intrevallo temporale tra le misure: %f]\n[Tempo tra le scansioni: %f]\n[Range massimo: %f]\n[Range minimo: %f]\n[%s]\n",msg->angle_min,msg->angle_max, msg->angle_increment,msg->time_increment,msg->scan_time,msg->range_max,msg->range_min,s.c_str());
+    Ricordare in questo caso di usare s.c_str() nella ROS_INFO per converitre la stringa in stringa c
+    */
+   ROS_INFO("\n[Angolo minimo: %f]\n[Angolo massimo: %f]\n[Passo: %f]\n[Intrevallo temporale tra le misure: %f]\n[Tempo tra le scansioni: %f]\n[Range massimo: %f]\n[Range minimo: %f]\n[%f] [%f] [%f] [%f] [%f] [%f]",msg->angle_min,msg->angle_max, msg->angle_increment,msg->time_increment,msg->scan_time,msg->range_max,msg->range_min,min_set1,min_set2,min_set3,min_set4,min_set5,min_set6);
 }
 
 int main(int argc, char **argv)
